018968aba7cf41138022301e11c5c2f08987a5c1
Davi <14300037663@estudante.ufjf.br>
2022-06-28 19:47:51 -0300

    mostrar tipos de variaveis

---
+++ ex02.js
@@ -0,0 +1,13 @@
+var a = 10;
+console.log(a);
+
+var b = "Olá Mundo"
+console.log(b)
+
+var c = true;
+console.log(c)
+
+var d;
+var e = null
+var f = function(){}
+console.log( typeof a, typeof b, typeof c, typeof d, typeof e, typeof f)
